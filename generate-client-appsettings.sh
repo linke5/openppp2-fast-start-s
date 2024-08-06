eba5f5de79d81dbf12b914e44734a37d7966a045
Ling|陵云溪 <82665891+linke5@users.noreply.github.com>
2024-08-07 01:21:34 +0800

    support client config generate

--- generate-client-appsettings.sh
+++ generate-client-appsettings.sh
@@ -28,64 +28,125 @@ read_config_file() {
 }
 
 check_ip() {
-    ip_out=$(curl -s -4 https://get.geojs.io/v1/ip.json)
+    organization_name=("Google" "Amazon" "Azure" "Alibaba" "Tencent" "Huawei")
+    
+    ip_out=$(curl -s -4 https://get.geojs.io/v1/ip)
     ip_local=($(ip a | awk '/inet / {print $2}' | cut -d/ -f1))
     unspecified_ips=("0.0.0.0" "::")
-    if [[ $ip_config == ${unspecified_ips[@]} ]]; then
-        ip_config=$(echo "$ip_out" | jq -r '.ip')
-        echo "因未指定默认监听 IP "
-        echo "连接 IP 将配置为 $ip_config "
-    elif [[ $ip_config ==  ${ip_local[@]} ]]; then
-        echo "监听 IP 与接口 IP 相同"
-        echo "连接 IP 将配置为 $ip_config "
     
+    if [[ " ${unspecified_ips[*]} " == *" $ip_config "* ]]; then
+        ip_config=$ip_out
+        echo "因未指定默认监听 IP "
+        echo "连接 IP 将配置为出口 IP $ip_config"
+    elif [[ " ${ip_local[*]} " == *" $ip_config "* ]]; then
+        echo "监听 IP 与接口 IP 相同"
+        echo "连接 IP 将配置为 $ip_config"
+    else
+        echo "出口 IP 与 接口 IP 皆不符合监听 IP"
+        
+        ip_out_geo=$(curl -s -4 https://get.geojs.io/v1/ip/geo.json | jq -r '.organization_name')
+        pattern=$(IFS="|"; echo "${organization_name[*]}")
+        
+        if echo "$ip_out_geo" | grep -iqE "$pattern"; then
+            echo "检测到 VPC 架构 默认使用出口 IP"
+            ip_config=$ip_out
+            echo "连接 IP 将配置为 $ip_config"
+        else
+            echo "无法正确判断连接IP"
+            echo "请选择连接 IP"
+            echo "1.接口 IP"
+            echo "2.出口 IP"
+            read -p "请选择连接 IP (1/2): " customize_ip
+            
+            if [[ $customize_ip == 1 ]]; then
+                echo "将使用接口 IP 作为连接 IP"
+                ip_config=$ip_local
+            elif [[ $customize_ip == 2 ]]; then
+                echo "将使用出口 IP 作为连接 IP"
+                ip_config=$ip_out
+            else
+                echo "默认使用出口 IP 作为连接 IP"
+                ip_config=$ip_out
+            fi
+        fi
+    fi
 }
 
-generate_config_file() {
-
-
-    jq --arg concurrent "$concurrent" \
-       --arg protocol "$protocol" \
-       --arg protocol_key "$protocol_key" \
-       --arg transport "$transport" \
-       --arg transport_key "$transport_key" \
-       --arg ip_config "$ip_config" \
-       --arg listen_port_tcp_ppp "$listen_port_tcp_ppp" \
-       --arg listen_port_udp_ppp "$listen_port_udp_ppp" \
-       --arg listen_port_tcp_ws "$listen_port_tcp_ws" \
-       --arg listen_port_tcp_wss "$listen_port_tcp_wss" \
-       --arg ws_host "$ws_host" \
-       --arg ws_path "$ws_path" \
-       '.concurrent = ($concurrent | tonumber) |
-        .key.protocol = $protocol |
-        .key["protocol-key"] = $protocol_key |
-        .key.transport = $transport |
-        .key["transport-key"] = $transport_key |
-        .tcp.listen.port = ($listen_port_tcp_ppp | tonumber) |
-        .udp.listen.port = ($listen_port_udp_ppp | tonumber) |
-        .udp.static.servers = "\($new_ip):\()"
-        .websocket.host = $ws_host |
-        .websocket.path 
-        = $ws_path |
-       ' "$config_file" > "${config_file}.tmp"
+generate_config() {
+    check_ip
+    cat > $server_config_dir/client/appsettings.json << EOF
+{
+    "concurrent": 1,
+    "key": {
+        "kf": 154543927,
+        "kx": 128,
+        "kl": 10,
+        "kh": 12,
+        "protocol": "$protocol",
+        "protocol-key": "$transport_key",
+        "transport": "$transport",
+        "transport-key": "$transport_key",
+        "masked": false,
+        "plaintext": false,
+        "delta-encode": false,
+        "shuffle-data": false
+    },
+    "ip": {
+        "public": "::",
+        "interface": "::"
+    },
+    "tcp": {
+        "inactive": {
+            "timeout": 300
+        },
+        "connect": {
+            "timeout": 5
+        },
+        "listen": {
+            "port": 20000
+        },
+        "turbo": true,
+        "backlog": 511,
+        "fast-open": true
+    },
+    "udp": {
+        "inactive": {
+            "timeout": 72
+        },
+        "dns": {
+            "timeout": 4,
+            "redirect": "0.0.0.0"
+        },
+        "listen": {
+            "port": 20000
+        },
+        "static": {
+            "keep-alived": [ 300, 600 ],
+            "dns": true,
+            "quic": true,
+            "icmp": true,
+            "aggligator": 0,
+            "servers": [ "$ip_config:$listen_port_udp_ppp" ]
+        }
+    },
+    "client": {
+        "guid": "{$(cat /proc/sys/kernel/random/uuid)}",
+        "server": "ppp://$ip_config:$listen_port_tcp_ppp/",
+        "reconnections": {
+            "timeout": 5
+        },        
+        "http-proxy": {
+            "bind": "127.0.0.1",
+            "port": 7890
+        }
+    }
+}
+EOF
+echo "已存放在 $server_config_dir/client/appsettings.json "
+echo "Windows 运行指令: --mode=client --tun-ip=10.0.0.2 --tun-gw=10.0.0.0 --tun-mask=24 --tun-vnet=yes --tun-static=yes"
+echo "非Windows 运行指令: --mode=client --tun-ip=10.0.0.2 --tun-gw=10.0.0.1 --tun-mask=24 --tun-vnet=yes --tun-static=yes"
+echo "注: 每个客户端的 UUID 与 Tun-IP 应当为唯一值"
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+generate_config
 check_install_dependency jq "sudo apt update && sudo apt install -y jq"
